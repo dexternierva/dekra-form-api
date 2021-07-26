a78a6a879e6800a6cfa2fc0c8af93bc4703ea71c
dexternierva <dexternierva@gmail.com>
2021-07-26 15:20:03 +0800

    Add middleware.js

---
+++ config/middleware.js
@@ -0,0 +1,14 @@
+module.exports = () => ({
+  settings: {
+    cors: {
+      enabled: true,
+      origin: "https://dekra-form-8a7iy.ondigitalocean.app, https://dekra-form-api-m8bsw.ondigitalocean.app",
+      headers: [
+        "Content-Type",
+        "Authorization",
+        "X-Frame-Options",
+        "access-control-allow-origin"
+      ]
+    },
+  },
+});
