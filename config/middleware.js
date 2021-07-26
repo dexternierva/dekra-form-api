26e30c5785339fefa90361df59ed9b6e31b01769
dexternierva <dexternierva@gmail.com>
2021-07-26 12:10:33 +0800

    Update URL on the middleware.js file

--- config/middleware.js
+++ config/middleware.js
@@ -2,7 +2,7 @@ module.exports = () => ({
   settings: {
     cors: {
       enabled: true,
-      origin: "http://localhost:3000, http://localhost:1337",
+      origin: "http://localhost:3000, https://dekra-form-api-m8bsw.ondigitalocean.app/",
       headers: [
         "Content-Type",
         "Authorization",
