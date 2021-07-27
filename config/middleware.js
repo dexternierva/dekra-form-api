33627e5c895a44439952f2787a563bba32ad5b6c
dexternierva <dexternierva@gmail.com>
2021-07-27 12:53:20 +0800

    Add credentials: true to middleware.js

--- config/middleware.js
+++ config/middleware.js
@@ -2,6 +2,7 @@ module.exports = () => ({
   settings: {
     cors: {
       enabled: true,
+      credentials: true,
       origin: "https://dekra-form-8a7iy.ondigitalocean.app, https://dekra-form-api-m8bsw.ondigitalocean.app",
       headers: [
         "Content-Type",
