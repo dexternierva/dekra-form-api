7a22251d9b881ae88eb833dabea6f94d5c0a31d1
dexternierva <dexternierva@gmail.com>
2021-07-27 22:00:39 +0800

    Moved middleware.js inside production folder

---
+++ config/env/production/middleware.js
@@ -0,0 +1,15 @@
+module.exports = () => ({
+  settings: {
+    cors: {
+      enabled: true,
+      credentials: true,
+      origin: "https://dekra-form-8a7iy.ondigitalocean.app, https://dekra-form-api-m8bsw.ondigitalocean.app",
+      headers: [
+        "Content-Type",
+        "Authorization",
+        "X-Frame-Options",
+        "access-control-allow-origin"
+      ]
+    },
+  },
+});
