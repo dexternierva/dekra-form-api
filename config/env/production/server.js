8071703e0e3a0d957f4a301c02f69951462ff3df
dexternierva <dexternierva@gmail.com>
2021-07-27 10:17:42 +0800

    Added 'proxy: true' in the server.js file

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,3 +1,4 @@
 module.exports = ({ env }) => ({
+	proxy: true,
 	url: env('STRAPI_URL')
 });
--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  proxy: true,
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'ef8e7db1a58b0884ed2bf5a179460149'),
