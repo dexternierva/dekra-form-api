302eaa806d70d73b1fbde1de8e063e195b5a2f56
dexternierva <dexternierva@gmail.com>
2021-07-27 14:21:08 +0800

    Update PRODUCTION_URL value inside Auth.js

--- extensions/users-permissions/controllers/Auth.js
+++ extensions/users-permissions/controllers/Auth.js
@@ -145,7 +145,7 @@ module.exports = {
           httpOnly: true,
           secure: process.env.NODE_ENV === "production" ? true : false,
           maxAge: 1000 * 60 * 60 * 24 * 14, // 14 Day Age
-          domain: process.env.NODE_ENV === "development" ? "localhost" : process.env.APP_URL,
+          domain: process.env.NODE_ENV === "development" ? "localhost" : "https://dekra-form-8a7iy.ondigitalocean.app/",
         });
 
         ctx.send({
