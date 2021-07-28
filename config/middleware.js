1c404aaffe90d1675813f572ca89b554acd03684
dexternierva <dexternierva@gmail.com>
2021-07-28 21:16:13 +0800

    Overhauled Auth, permissions and middleware

--- config/middleware.js
+++ config/middleware.js
@@ -1,16 +1,77 @@
-module.exports = () => ({
+module.exports = ({ env }) => ({
+  load: {
+    before: [
+      "cookieGetter",
+      "responseTime",
+      "logger",
+      "cors",
+      "responses",
+      "gzip",
+    ],
+    order: [
+      "cookieGetter", "cookieSetter"
+    ],
+    after: ["parser", "router", "cookieSetter"],
+  },
   settings: {
     cors: {
+      origin: [
+        env("CLIENT_URL", "https://dekra-form-8a7iy.ondigitalocean.app"),
+        env("API_URL", "https://dekra-form-api-m8bsw.ondigitalocean.app"),
+      ],
+    },
+    cookieGetter: {
       enabled: true,
-      credentials: true,
-      origin: ['https://dekra-form-8a7iy.ondigitalocean.app', 'https://dekra-form-api-m8bsw.ondigitalocean.app'],
-      headers: [
-        "Content-Type",
-        "Authorization",
-        "X-Frame-Options",
-        "access-control-allow-origin"
-      ]
+    },
+    cookieSetter: {
+      enabled: true,
     },
   },
 });
+
+// cookieSetter
+module.exports = (strapi) => {
+  return {
+    initialize() {
+      strapi.app.use(async (ctx, next) => {
+        await next();
+        if (
+          ctx.request.url.startsWith("/auth/") &&
+          ctx.response.status === 200
+        ) {
+          const { jwt: jwtToken } = ctx.response.body;
+          ctx.cookies.set("token", jwtToken, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            maxAge: 1000 * 60 * 60 * 24 * 14, // 14 Day Age
+            domain: "dekra-form-8a7iy.ondigitalocean.app",
+            sameSite: process.env.NODE_ENV === "development" ? true : "none",
+            overwrite: true,
+          });
+        }
+      });
+    },
+  };
+};
+
+// cookieGetter
+module.exports = (strapi) => {
+  return {
+    initialize() {
+      strapi.app.use(async (ctx, next) => {
+        if (
+          ctx.request &&
+          ctx.request.header &&
+          !ctx.request.header.authorization
+        ) {
+          const token = ctx.cookies.get("token");
+          if (token) {
+            ctx.request.header.authorization = `Bearer ${token}`;
+          }
+        }
+        await next();
+      });
+    },
+  };
+};
 
