2128854b06b555630e3837179f2413b734224ada
dexternierva <dexternierva@gmail.com>
2021-07-29 14:32:40 +0800

    Overhauled creation of jwt tokens

--- config/middleware.js
+++ config/middleware.js
@@ -1,28 +1,9 @@
 module.exports = ({ env }) => ({
-  load: {
-    before: [
-      "cookieGetter",
-      "responseTime",
-      "logger",
-      "cors",
-      "responses",
-      "gzip",
-    ],
-    order: [
-      "Define the middlewares' load order by putting their name in this array is the right order",
-    ],
-    after: ["parser", "router", "cookieSetter"],
-  },
   settings: {
     cors: {
+      enabled: true,
       origin: ['https://dekra-form-api-m8bsw.ondigitalocean.app', 'https://dekra-form-8a7iy.ondigitalocean.app'],
-    },
-    cookieGetter: {
-      enabled: true,
-    },
-    cookieSetter: {
-      enabled: true,
-    },
+    }
   },
 });
 
--- extensions/users-permissions/config/policies/permissions.js
+++ extensions/users-permissions/config/policies/permissions.js
@@ -1,105 +1,105 @@
-// 'use strict';
+'use strict';
 
-// const _ = require('lodash');
+const _ = require('lodash');
 
-// module.exports = async (ctx, next) => {
-//   let role;
+module.exports = async (ctx, next) => {
+  let role;
 
-//   if (ctx.state.user) {
-//     // request is already authenticated in a different way
-//     return next();
-//   }
+  if (ctx.state.user) {
+    // request is already authenticated in a different way
+    return next();
+  }
 
-//   /** START OF CUSTOMIZATION */
-//   if (ctx.request && ctx.request.header && !ctx.request.header.authorization) {
-//     console.log("ctx.cookies.get('token') inside permissions.js: ", ctx.cookies.get("token"));
-//     const token = ctx.cookies.get("token");
-//     if (token) {
-//       ctx.request.header.authorization = "Bearer " + token;
-//     }
+  /** START OF CUSTOMIZATION */
+  if (ctx.request && ctx.request.header && !ctx.request.header.authorization) {
+    console.log("ctx.cookies.get('token') inside permissions.js: ", ctx.cookies.get("token"));
+    const token = ctx.cookies.get("token");
+    if (token) {
+      ctx.request.header.authorization = "Bearer " + token;
+    }
 
-//     console.log("token variable inside permissions.js: ", token);
-//   }
-//   /** END OF CUSTOMIZATION */
+    console.log("token variable inside permissions.js: ", token);
+  }
+  /** END OF CUSTOMIZATION */
 
-//   if (ctx.request && ctx.request.header && ctx.request.header.authorization) {
-//     try {
-//       const { id } = await strapi.plugins['users-permissions'].services.jwt.getToken(ctx);
+  if (ctx.request && ctx.request.header && ctx.request.header.authorization) {
+    try {
+      const { id } = await strapi.plugins['users-permissions'].services.jwt.getToken(ctx);
 
-//       if (id === undefined) {
-//         throw new Error('Invalid token: Token did not contain required fields');
-//       }
+      if (id === undefined) {
+        throw new Error('Invalid token: Token did not contain required fields');
+      }
 
-//       // fetch authenticated user
-//       ctx.state.user = await strapi.plugins[
-//         'users-permissions'
-//       ].services.user.fetchAuthenticatedUser(id);
-//     } catch (err) {
-//       return handleErrors(ctx, err, 'unauthorized');
-//     }
+      // fetch authenticated user
+      ctx.state.user = await strapi.plugins[
+        'users-permissions'
+      ].services.user.fetchAuthenticatedUser(id);
+    } catch (err) {
+      return handleErrors(ctx, err, 'unauthorized');
+    }
 
-//     if (!ctx.state.user) {
-//       return handleErrors(ctx, 'User Not Found', 'unauthorized');
-//     }
+    if (!ctx.state.user) {
+      return handleErrors(ctx, 'User Not Found', 'unauthorized');
+    }
 
-//     role = ctx.state.user.role;
+    role = ctx.state.user.role;
 
-//     if (role.type === 'root') {
-//       return await next();
-//     }
+    if (role.type === 'root') {
+      return await next();
+    }
 
-//     const store = await strapi.store({
-//       environment: '',
-//       type: 'plugin',
-//       name: 'users-permissions',
-//     });
+    const store = await strapi.store({
+      environment: '',
+      type: 'plugin',
+      name: 'users-permissions',
+    });
 
-//     if (
-//       _.get(await store.get({ key: 'advanced' }), 'email_confirmation') &&
-//       !ctx.state.user.confirmed
-//     ) {
-//       return handleErrors(ctx, 'Your account email is not confirmed.', 'unauthorized');
-//     }
+    if (
+      _.get(await store.get({ key: 'advanced' }), 'email_confirmation') &&
+      !ctx.state.user.confirmed
+    ) {
+      return handleErrors(ctx, 'Your account email is not confirmed.', 'unauthorized');
+    }
 
-//     if (ctx.state.user.blocked) {
-//       return handleErrors(
-//         ctx,
-//         'Your account has been blocked by the administrator.',
-//         'unauthorized'
-//       );
-//     }
-//   }
+    if (ctx.state.user.blocked) {
+      return handleErrors(
+        ctx,
+        'Your account has been blocked by the administrator.',
+        'unauthorized'
+      );
+    }
+  }
 
-//   // Retrieve `public` role.
-//   if (!role) {
-//     role = await strapi.query('role', 'users-permissions').findOne({ type: 'public' }, []);
-//   }
+  // Retrieve `public` role.
+  if (!role) {
+    role = await strapi.query('role', 'users-permissions').findOne({ type: 'public' }, []);
+  }
 
-//   const route = ctx.request.route;
-//   const permission = await strapi.query('permission', 'users-permissions').findOne(
-//     {
-//       role: role.id,
-//       type: route.plugin || 'application',
-//       controller: route.controller,
-//       action: route.action,
-//       enabled: true,
-//     },
-//     []
-//   );
+  const route = ctx.request.route;
+  const permission = await strapi.query('permission', 'users-permissions').findOne(
+    {
+      role: role.id,
+      type: route.plugin || 'application',
+      controller: route.controller,
+      action: route.action,
+      enabled: true,
+    },
+    []
+  );
 
-//   if (!permission) {
-//     return handleErrors(ctx, undefined, 'forbidden');
-//   }
+  if (!permission) {
+    return handleErrors(ctx, undefined, 'forbidden');
+  }
 
-//   // Execute the policies.
-//   if (permission.policy) {
-//     return await strapi.plugins['users-permissions'].config.policies[permission.policy](ctx, next);
-//   }
+  // Execute the policies.
+  if (permission.policy) {
+    return await strapi.plugins['users-permissions'].config.policies[permission.policy](ctx, next);
+  }
 
-//   // Execute the action.
-//   await next();
-// };
+  // Execute the action.
+  await next();
+};
 
-// const handleErrors = (ctx, err = undefined, type) => {
-//   throw strapi.errors[type](err);
-// };
+const handleErrors = (ctx, err = undefined, type) => {
+  throw strapi.errors[type](err);
+};
