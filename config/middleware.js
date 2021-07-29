e170a90226b08ce0a2f943b6f76a466bbc4e70a4
dexternierva <dexternierva@gmail.com>
2021-07-29 09:48:55 +0800

    Console variables in middleware.js

--- config/middleware.js
+++ config/middleware.js
@@ -39,12 +39,16 @@ module.exports = (strapi) => {
           const { jwt: jwtToken } = ctx.response.body;
           ctx.cookies.set("token", jwtToken, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === "production",
+            secure: process.env.NODE_ENV === "production" ? true : false,
             maxAge: 1000 * 60 * 60 * 24 * 14, // 14 Day Age
             domain: process.env.CLIENT_HOSTNAME,
             sameSite: process.env.NODE_ENV === "development" ? true : "none",
             overwrite: true,
           });
+
+          console.log("*** process.env: ", process.env);
+          console.log("*** jwt: ", jwt);
+          console.log("*** jwtToken: ", jwtToken);
         }
       });
     },
