e5e6200d6da5649baa0a5e2f6d47eac42b609361
dexternierva <dexternierva@gmail.com>
2021-07-29 10:28:20 +0800

    Configure middlewares add cookieSetter and cookieGetter

--- config/middleware.js
+++ config/middleware.js
@@ -15,7 +15,7 @@ module.exports = ({ env }) => ({
   },
   settings: {
     cors: {
-      origin: ['https://dekra-form-api-m8bsw.ondigitalocean.app/', 'https://dekra-form-8a7iy.ondigitalocean.app/'],
+      origin: ['https://dekra-form-api-m8bsw.ondigitalocean.app', 'https://dekra-form-8a7iy.ondigitalocean.app'],
     },
     cookieGetter: {
       enabled: true,
@@ -26,53 +26,7 @@ module.exports = ({ env }) => ({
   },
 });
 
-// cookieSetter
-module.exports = (strapi) => {
-  return {
-    initialize() {
-      strapi.app.use(async (ctx, next) => {
-        await next();
-        if (
-          ctx.request.url.startsWith("/auth/") &&
-          ctx.response.status === 200
-        ) {
-          const { jwt: jwtToken } = ctx.response.body;
-          ctx.cookies.set("token", jwtToken, {
-            httpOnly: true,
-            secure: process.env.NODE_ENV === "production" ? true : false,
-            maxAge: 1000 * 60 * 60 * 24 * 14, // 14 Day Age
-            domain: process.env.CLIENT_HOSTNAME,
-            sameSite: process.env.NODE_ENV === "development" ? true : "none",
-            overwrite: true,
-          });
 
-          console.log("*** process.env: ", process.env);
-          console.log("*** jwt: ", jwt);
-          console.log("*** jwtToken: ", jwtToken);
-        }
-      });
-    },
-  };
-};
 
-// cookieGetter
-module.exports = (strapi) => {
-  return {
-    initialize() {
-      strapi.app.use(async (ctx, next) => {
-        if (
-          ctx.request &&
-          ctx.request.header &&
-          !ctx.request.header.authorization
-        ) {
-          const token = ctx.cookies.get("token");
-          if (token) {
-            ctx.request.header.authorization = `Bearer ${token}`;
-          }
-        }
-        await next();
-      });
-    },
-  };
-};
+
 
---
+++ middlewares/cookieSetter/index.js
@@ -0,0 +1,27 @@
+module.exports = (strapi) => {
+  return {
+    initialize() {
+      strapi.app.use(async (ctx, next) => {
+        await next();
+        if (
+          ctx.request.url.startsWith("/auth/") &&
+          ctx.response.status === 200
+        ) {
+          const { jwt: jwtToken } = ctx.response.body;
+          ctx.cookies.set("token", jwtToken, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production" ? true : false,
+            maxAge: 1000 * 60 * 60 * 24 * 14, // 14 Day Age
+            domain: process.env.CLIENT_HOSTNAME,
+            sameSite: process.env.NODE_ENV === "development" ? true : "none",
+            overwrite: true,
+          });
+
+          console.log("*** process.env: ", process.env);
+          console.log("*** jwt: ", jwt);
+          console.log("*** jwtToken: ", jwtToken);
+        }
+      });
+    },
+  };
+};
