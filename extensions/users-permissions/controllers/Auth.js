bd35f3434d06d1a6d9dc77872e5f90634c4585c2
dexternierva <dexternierva@gmail.com>
2021-07-30 09:17:04 +0800

    Set domain to process.env.HOSTNAME

--- extensions/users-permissions/controllers/Auth.js
+++ extensions/users-permissions/controllers/Auth.js
@@ -145,7 +145,7 @@ module.exports = {
             httpOnly: true,
             secure: process.env.NODE_ENV === "production" ? true : false,
             maxAge: 1000 * 60 * 60 * 24 * 14, // 14 Day Age
-            domain: process.env.NODE_ENV === "development" ? "localhost" : process.env.APP_DOMAIN,
+            domain: process.env.HOSTNAME,
         });
 
         ctx.send({
