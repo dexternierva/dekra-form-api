32b94df379e5bc5299893c5e55ce5ec263794af7
dexternierva <dexternierva@gmail.com>
2021-08-02 21:59:49 +0800

    The Boom!

---
+++ api/training/controllers/training.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
+
+module.exports = {
+    async create(ctx) {
+        let entity;
+        if (ctx.is('multipart')) {
+            const { data, files } = parseMultipartData(ctx);
+            data.user = ctx.state.user.id;
+            entity = await strapi.services['training'].create(data, { files });
+        } else {
+            ctx.request.body.user = ctx.state.user.id;
+            entity = await strapi.services['training'].create(ctx.request.body);
+        }
+
+        return sanitizeEntity(entity, { model: strapi.models['training'] });
+    },
+};
